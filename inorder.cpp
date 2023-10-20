33d5c0ac7911497585e8196f1ca98e98536fe3e9
Hadas Cohen <hc820@live.mdx.ac.uk>
2023-10-20 13:50:20 +0100

    initial commit

---
+++ inorder.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int x,y,z;
+  cout << "Enter three integers: ";
+  cin >> x >> y >> z;
+  if((x < y && x < z)&&(y>x && y<z)){
+    cout << "In order";
+  }
+  else{
+    cout << "Not in order";
+
+    return 0;
+ }
