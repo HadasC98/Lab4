55bbda728c08b3760e8171a1124210a8a7d36e38
Hadas Cohen <hc820@live.mdx.ac.uk>
2024-01-30 17:00:07 +0000

    inital commit

---
+++ chartype.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int x,y,z;
+  cout << "Enter three character: ";
+//   cin >> x >> y >> z;
+//   if((x < y && x < z)&&(y>x && y<z)){
+//     cout << "In order";
+//   }
+//   else{
+//     cout << "Not in order";
+
+//     return 0;
+//  }
+}
